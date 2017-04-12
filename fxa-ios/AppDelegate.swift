dc37ca8e0a48c21e4ca8e998cc749cde80e05a25
Chenxia Liu <liuche@mozilla.com>
2017-04-11 17:39:54 -0700

    Skeleton build.

--- fxa-ios/AppDelegate.swift
+++ fxa-ios/AppDelegate.swift
@@ -1,10 +1,6 @@
-//
-//  AppDelegate.swift
-//  fxa-ios
-//
-//  Created by Chenxia Liu on 4/11/17.
-//  Copyright © 2017 Chenxia Liu. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import UIKit
 
--- fxa-iosTests/fxa_iosTests.swift
+++ fxa-iosTests/fxa_iosTests.swift
@@ -1,10 +1,6 @@
-//
-//  fxa_iosTests.swift
-//  fxa-iosTests
-//
-//  Created by Chenxia Liu on 4/11/17.
-//  Copyright © 2017 Chenxia Liu. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import XCTest
 @testable import fxa_ios
